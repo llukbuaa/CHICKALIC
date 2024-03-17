df784e31782a7097e2bcd7eea6412dfb61a5cd36
Phanthira Phansen <phanthira.pha@student.mahidol.edu>
2024-03-17 09:09:24 +0700

    menu description

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,14 @@
 
 import Navbar from "./component/navbar";
+import Menu from "./description/Menu";
 import Home from "./page/Home";
 
 function App() {
   return (
     // <Navbar />
-    <Home />
+    //<Home />
     // <h1 className='text-6xl'>CHICKALIC</h1>
+    <Menu />
     );
 }
 
